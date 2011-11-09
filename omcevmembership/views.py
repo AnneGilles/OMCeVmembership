03b894a794746d1ad051a54401121a5884d91528
Christoph Scheid <c@shri.de>
2011-11-09 01:57:10 +0100

    view join_membership

--- omcevmembership/views.py
+++ omcevmembership/views.py
@@ -1,7 +1,96 @@
 from omcevmembership.models import DBSession
 from omcevmembership.models import MyModel
 
+import formencode
+from pyramid_simpleform import Form
+from pyramid_simpleform.renderers import FormRenderer
+
+from fdfgen import forge_fdf
+from datetime import datetime
+
 def my_view(request):
     dbsession = DBSession()
     root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
     return {'root':root, 'project':'OMCeVmembership'}
+
+
+class MembershipSchema(formencode.Schema):
+    """
+    formencode schema for membership application form
+    """
+    allow_extra_fields = True
+    lastname = formencode.validators.PlainText(not_empty = True)
+    surname  = formencode.validators.String(not_empty = True)
+    lastname = formencode.validators.String(not_empty = True)
+    address1 = formencode.validators.String(not_empty = True)
+    address2 = formencode.validators.String(not_empty = True)
+    email =  formencode.validators.Email(not_empty = True)
+    phone =  formencode.validators.String(not_empty = True)
+
+
+def join_membership(request):
+
+    form = Form(request, schema = MembershipSchema)
+
+    if form.validate():
+        print "the form validated OK"
+
+    if 'form.submitted' in request.POST and form.validate():
+        print "form was submitted and validated OK."
+        print "membership_type: " + str(form.data['membership_type'])
+        if 'supporter' in form.data['membership_type']:
+            print "found 'supporter'"
+            membership_fee = 42
+            FoerderMitglied = True
+            OrdentlichesMitglied = False
+        else:
+            print "'supporter' not found"
+            membership_fee = 23
+            FoerderMitglied = 'Off'
+            OrdentlichesMitglied = 'On'
+
+        #print "request.POST: " + str(request.POST)
+
+        fields = [
+            ('Name', form.data['lastname']),
+            ('Surname', form.data['surname']),
+            ('Street', form.data['address1']),
+            ('PostCodeCity', form.data['address2']),
+            ('Telephone', form.data['phone']),
+            ('Email', form.data['email']),
+            ('OrdentlichesMitglied', OrdentlichesMitglied), # not working
+            ('FoerderMitglied', FoerderMitglied), # not working. < ToDo ^
+            ]
+        #generate fdf string
+        fdf = forge_fdf("", fields, [], [], [])
+        # write to file
+        my_fdf_filename = "fdf.fdf"
+        import os
+        fdf_file = open(my_fdf_filename , "w")
+        # fdf_file.write(fdf.encode('utf8'))
+        fdf_file.write(fdf)
+        fdf_file.close()
+
+        print os.popen('pdftk pdftk/beitrittserklaerung.pdf fill_form %s output formoutput.pdf flatten'% (my_fdf_filename)).read()
+
+        #print os.popen('pwd').read()
+        #print os.popen('ls').read()
+
+        # combine
+        print "combining with bank account form"
+        print os.popen('pdftk formoutput.pdf pdftk/bankaccount.pdf output combined.pdf').read()
+        print "combined personal form and bank form"
+
+
+        # return a pdf file
+        from pyramid.response import Response
+        response = Response(content_type='application/pdf')
+        response.app_iter = open("combined.pdf", "r")
+        return response
+
+
+
+
+    return {
+        'form': FormRenderer(form)
+        }
